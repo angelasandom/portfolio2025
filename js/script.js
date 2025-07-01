2d0fd585f259ac010460bb31639cd8d2a78ad2c2
angelasandom <angelasandom@gmail.com>
2025-07-01 16:31:14 +0100

    Improve Home copy and style

--- js/script.js
+++ js/script.js
@@ -40,26 +40,26 @@ navLinks.forEach(link => {
     });
 });
 
-//scroll reveal
-ScrollReveal({ 
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
+// //scroll reveal
+// ScrollReveal({ 
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200
+// });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .skills-container, projects.box, .contact-form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .skills-container, projects.box, .contact-form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 
-//typed js
- const typed = new Typed('.multiple-text', {
-   strings: ['UX Designer', 'Software Developer'],
-   typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-   loop: true
-})
+// //typed js
+//  const typed = new Typed('.multiple-text', {
+//    strings: ['UX Designer', 'Software Developer'],
+//    typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//    loop: true
+// })
 
 // const typed = new Typed('.multiple-text', {
 //     strings: ['Software Developer'],
